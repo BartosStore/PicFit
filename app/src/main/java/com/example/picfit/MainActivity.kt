c4aa8469666e8286d52cda4180bc96444e737c78
Miroslav Bartoš <mirek-b@seznam.cz>
2019-07-29 20:12:14 +0200

    open camera on button click

--- app/src/main/java/com/example/picfit/MainActivity.kt
+++ app/src/main/java/com/example/picfit/MainActivity.kt
@@ -1,19 +1,30 @@
 package com.example.picfit
 
+import android.content.Intent
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.provider.MediaStore
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val CAMERA_REQUEST_CODE = 0
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Camera ON", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        fab.setOnClickListener {
+            dispatchTakePictureIntent()
+        }
+    }
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE)
         }
     }
 }
+
